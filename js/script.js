d9048082a255ab9a99857fdbc4c18cb28480ce62
MashaDvornyk <153934095+MashaDvornyk@users.noreply.github.com>
2023-12-21 15:02:34 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.navigation_btn');
+const menu = document.querySelector('.navigation_list');
+
+	hamburger.addEventListener('click', () =>{
+    	menu.classList.toggle('menu-open');
+	});
+	
